fd19c39b9ce0d9a91a48a9bc18e25385c4d0742e
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2024-10-09 16:14:26 +0200

    dockerfile: device support with frontend attribute
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- frontend/dockerfile/dockerfile_test.go
+++ frontend/dockerfile/dockerfile_test.go
@@ -300,6 +300,11 @@ func TestIntegration(t *testing.T) {
 			"granted": networkHostGranted,
 			"denied":  networkHostDenied,
 		}))...)
+
+	integration.Run(t, deviceTests, append(opts,
+		integration.WithMatrix("cdi", map[string]interface{}{
+			"enabled": enableCDI,
+		}))...)
 }
 
 func testEmptyStringArgInEnv(t *testing.T, sb integration.Sandbox) {
@@ -9849,6 +9854,19 @@ func (*networkModeSandbox) UpdateConfigFile(in string) string {
 var (
 	networkHostGranted integration.ConfigUpdater = &networkModeHost{}
 	networkHostDenied  integration.ConfigUpdater = &networkModeSandbox{}
+)
+
+type cdiEnabled struct{}
+
+func (*cdiEnabled) UpdateConfigFile(in string) string {
+	return in + `
+[cdi]
+enabled = true
+`
+}
+
+var (
+	enableCDI integration.ConfigUpdater = &cdiEnabled{}
 )
 
 func fixedWriteCloser(wc io.WriteCloser) filesync.FileOutputFunc {
